f5664f5b05a305ca40ac91ea577cd1314c881125
abeer-hizucc <marksmanabr@gmail.com>
2024-02-27 17:50:26 +0600

    added update and find by id

--- controllers/books.go
+++ controllers/books.go
@@ -22,3 +22,39 @@ func FindBooks(c *gin.Context) {
 	// Send the books as a JSON response
 	c.JSON(http.StatusOK, gin.H{"data": books})
 }
+func CreateBook(c *gin.Context) {
+	//Validate input
+	var input models.CreateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	book := models.Book{
+		Title:  input.Title,
+		Author: input.Author,
+	}
+	models.DB.Create(&book)
+	c.JSON(http.StatusOK, gin.H{"data": "Book Created Successfully"})
+}
+func FindBook(c *gin.Context) {
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+func UpdateBook(c *gin.Context) {
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+	var input models.CreateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	models.DB.Model(&book).Updates(input)
+	c.JSON(http.StatusOK, gin.H{"data": "Book has been successfully updated"})
+}
--- main.go
+++ main.go
@@ -10,5 +10,8 @@ func main() {
 	r := gin.Default()
 	models.ConnectDatabase()
 	r.GET("/books", controllers.FindBooks)
+	r.POST("/createBook", controllers.CreateBook)
+	r.GET("/books/:id", controllers.FindBook)
+	r.PUT("/books/:id", controllers.UpdateBook)
 	r.Run()
 }
